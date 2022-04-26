7fa8a443adba8727e12cdcff74d68653c6fdddd2
moreirak14 <kaiquemoreiracloud@gmail.com>
2022-04-26 10:08:48 -0300

    feat: add settings database uri

--- config.py
+++ config.py
@@ -1,4 +1,5 @@
 from pathlib import Path
+
 from dynaconf import Dynaconf
 
 PATH_ROOT = Path(__file__).parent
@@ -6,9 +7,23 @@ PATH_ROOT = Path(__file__).parent
 settings = Dynaconf(
     environments=True,
     envvar_prefix="CONNECTOR-GOOGLE-DV-360",
-    settings_files=['settings.toml', '.secrets.toml'],
-    includes=[f"{PATH_ROOT}/settings.toml", f"{PATH_ROOT}/.secret.toml"],
+    settings_files=["settings.toml", ".secrets.toml"],
+    includes=[f"{PATH_ROOT}/settings.toml", f"{PATH_ROOT}/.secrets.toml"],
 )
 
 # `envvar_prefix` = export envvars with `export DYNACONF_FOO=bar`.
 # `settings_files` = Load these files in the order.
+
+
+def database_uri():
+    _database_uri = (
+        f"{settings.database_dialect_driver}://"
+        f"{settings.database_user}:{settings.database_password}@"
+        f"{settings.database_host}:{settings.database_port}/"
+        f"{settings.database_name}"
+    )
+
+    return _database_uri
+
+
+settings.database_uri = database_uri()
